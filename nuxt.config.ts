d42717c2ec0afea0b9894afd532720df579f946f
nOn1i <68577051+nOn1i@users.noreply.github.com>
2026-07-01 04:32:54 -0400

    Add files via upload

---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    'nuxt-auth-utils'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  ignore: ['app/generated/**'],
+
+  nitro: {
+    externals: {
+      external: ['@prisma/client', '@prisma/adapter-mariadb']
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
